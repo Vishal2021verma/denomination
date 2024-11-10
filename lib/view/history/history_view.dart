7ca2deb04b2fdfd61cdd2fdccf5811b642db92a9
Vishal2021verma <black2020panther@gmail.com>
2024-11-11 02:09:40 +0530

    feat : get all items implemented on history view

--- lib/view/history/history_view.dart
+++ lib/view/history/history_view.dart
@@ -1,5 +1,11 @@
+import 'package:denomination/data/model/calculation_item.dart';
+import 'package:denomination/utils/format_indian_number_system.dart';
+import 'package:denomination/view/history/provider/history_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:indian_currency_to_word/indian_currency_to_word.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class HistoryView extends StatefulWidget {
   const HistoryView({super.key});
@@ -9,88 +15,133 @@ class HistoryView extends StatefulWidget {
 }
 
 class _HistoryViewState extends State<HistoryView> {
+  final converter = AmountToWords();
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<HistoryProvider>(context, listen: false).getItems();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("History"),
       ),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemBuilder: (context, index) => Slidable(
-              key: const ValueKey(0),
-              startActionPane: ActionPane(
-                motion: const ScrollMotion(),
-                children: [
-                  SlidableAction(
-                    onPressed: (value) {
-                      //Implement Delete Feature
-                    },
-                    backgroundColor: const Color(0xFFFE4A49),
-                    foregroundColor: Colors.white,
-                    icon: Icons.delete,
+      body: Consumer<HistoryProvider>(
+        builder: (context, provider, _) {
+          return ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemBuilder: (context, index) => Slidable(
+                  key: const ValueKey(0),
+                  startActionPane: ActionPane(
+                    motion: const ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: (value) {
+                          //Implement Delete Feature
+                        },
+                        backgroundColor: const Color(0xFFFE4A49),
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                      ),
+                      SlidableAction(
+                        onPressed: (value) {
+                          //Implement Edit Feature
+                        },
+                        backgroundColor: const Color(0xFF21B7CA),
+                        foregroundColor: Colors.white,
+                        icon: Icons.edit,
+                      ),
+                      SlidableAction(
+                        onPressed: (value) {
+                          //Implement Share feature
+                        },
+                        backgroundColor: const Color(0xFF21CA46),
+                        foregroundColor: Colors.white,
+                        icon: Icons.share,
+                      ),
+                    ],
                   ),
-                  SlidableAction(
-                    onPressed: (value) {
-                      //Implement Edit Feature
-                    },
-                    backgroundColor: const Color(0xFF21B7CA),
-                    foregroundColor: Colors.white,
-                    icon: Icons.edit,
-                  ),
-                  SlidableAction(
-                    onPressed: (value) {
-                      //Implement Share feature
-                    },
-                    backgroundColor: const Color(0xFF21CA46),
-                    foregroundColor: Colors.white,
-                    icon: Icons.share,
-                  ),
-                ],
-              ),
-              child: listItem()),
-          itemCount: 10),
+                  child: listItem(provider.calculationItems[index])),
+              itemCount: provider.calculationItems.length);
+        },
+      ),
     );
   }
 
-  Widget listItem() {
+  Widget listItem(CalculationItem calculationItem) {
     return Card(
       color: const Color.fromARGB(255, 23, 28, 32),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: const Padding(
-        padding: EdgeInsets.all(6),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("General"),
+            Text(calculationItem.type ?? ""),
+
             Row(
               children: [
+                //Amount
                 Expanded(
                     child: Text(
-                  "₹ 10000 00",
-                  style: TextStyle(color: Colors.blue, fontSize: 24),
+                  "₹ ${FormatIndianNumberSystem.formatIndianNumber(double.parse(calculationItem.totalAmount ?? "0").toInt())}",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(
+                      color: calculationItem.type == "General"
+                          ? Colors.blue
+                          : calculationItem.type == "Income"
+                              ? Colors.green
+                              : Colors.red,
+                      fontSize: 24),
                 )),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
+                    //Date
                     Text(
-                      "Nov 10, 2024",
-                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                      (() {
+                        try {
+                          DateTime dateTime =
+                              DateTime.parse(calculationItem.date ?? '');
+                          String formattedDate =
+                              DateFormat('MMM dd, yyyy').format(dateTime);
+                          return formattedDate;
+                        } catch (e) {
+                          return "";
+                        }
+                      }()),
+                      style: const TextStyle(color: Colors.grey, fontSize: 12),
                     ),
+                    //Time
                     Text(
-                      "08:22 AM",
-                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                      (() {
+                        try {
+                          DateTime dateTime =
+                              DateTime.parse(calculationItem.date ?? '');
+                          String formattedDate =
+                              DateFormat('hh:mm a').format(dateTime);
+                          return formattedDate;
+                        } catch (e) {
+                          return "";
+                        }
+                      }()),
+                      style: const TextStyle(color: Colors.grey, fontSize: 12),
                     ),
                   ],
                 )
               ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 4,
             ),
+            //Remark
             Text(
-              "Remark",
-              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500),
+              calculationItem.remark ?? "",
+              style: const TextStyle(
+                  color: Colors.grey, fontWeight: FontWeight.w500),
             ),
           ],
         ),
--- lib/view/history/provider/history_provider.dart
+++ lib/view/history/provider/history_provider.dart
@@ -1,7 +1,24 @@
+import 'dart:developer';
+
+import 'package:denomination/data/model/calculation_item.dart';
 import 'package:denomination/data/repository/calculation_item_repository.dart';
 import 'package:flutter/material.dart';
 
 class HistoryProvider extends ChangeNotifier {
-   final CalculationItemRepository _calculationItemRepository;
-  HistoryProvider({required CalculationItemRepository calculationItemRepository}) : _calculationItemRepository = calculationItemRepository; 
+  final CalculationItemRepository _calculationItemRepository;
+  HistoryProvider(
+      {required CalculationItemRepository calculationItemRepository})
+      : _calculationItemRepository = calculationItemRepository;
+
+  List<CalculationItem> _calculationItems = [];
+  List<CalculationItem> get calculationItems => _calculationItems;
+
+  void getItems() {
+    try {
+      _calculationItems = _calculationItemRepository.getItems();
+      notifyListeners();
+    } catch (e) {
+      log(e.toString());
+    }
+  }
 }
